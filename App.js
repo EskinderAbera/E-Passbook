ddd51d2006ed68b43d9b64696dadf124e68dbe73
HawiTad <adugtad@gmail.com>
2023-02-23 11:47:28 +0300

    latest change

--- App.js
+++ App.js
@@ -5,10 +5,8 @@ import { ContextProvider } from "./Contexts/ContextProvider";
 import { NativeBaseProvider } from "native-base";
 import { Provider } from "react-redux";
 import store from "./store";
-import setUpInterceptor from "./lib/axios_interceptors";
 
 export default function App() {
-  setUpInterceptor({ store });
   return (
     <ContextProvider>
       <Provider store={store}>
